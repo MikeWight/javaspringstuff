298c4c932799cfaab0ce7e8a76dbed624ddd6664
MikeWight <michel.wight@hotmail.com>
2022-09-20 23:09:55 -0300

    added firebase. not working though

---
+++ demo/src/main/java/com/example/demo/firebase/FBInitialize.java
@@ -0,0 +1,32 @@
+package com.example.demo.firebase;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.io.FileInputStream;
+
+@Service
+public class FBInitialize {
+
+    @PostConstruct
+    public void initialize() {
+        try {
+            FileInputStream serviceAccount =
+                    new FileInputStream("./ffi.json");
+
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .setDatabaseUrl("https://reftest-7d527-default-rtdb.firebaseio.com")
+                    .build();
+
+            FirebaseApp.initializeApp(options);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
