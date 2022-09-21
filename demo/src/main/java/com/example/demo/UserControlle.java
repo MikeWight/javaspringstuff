be48df44baee929f25b5b6a9771b0ee7f41f3807
MikeWight <michel.wight@hotmail.com>
2022-09-20 21:07:38 -0300

    added another endpoint for fun

--- demo/src/main/java/com/example/demo/UserControlle.java
+++ demo/src/main/java/com/example/demo/UserControlle.java
@@ -5,15 +5,34 @@ import java.util.ArrayList;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.models.Dudes;
+
 @RestController
 public class UserControlle {
     
-    // Aggregate root
-  // tag::get-aggregate-root[]
   @GetMapping("/employees")
   ArrayList<String> all() {
     ArrayList<String> thing = new ArrayList<String>();
     thing.add("Hey");
+    thing.add("What");
+    return thing;
+  }
+
+  @GetMapping("/dudes")
+  ArrayList<Dudes> dudes() {
+    ArrayList<Dudes> thing = new ArrayList<Dudes>();
+    Dudes bud = new Dudes();
+    bud.setAge(20);
+    bud.setGroup("Super Dudes");
+    bud.setHeight("4' 9''");
+    bud.setName("Bert");
+    thing.add(bud);
+    Dudes bud2 = new Dudes();
+    bud2.setAge(22);
+    bud2.setGroup("Super Dudes");
+    bud2.setHeight("4' 10''");
+    bud2.setName("Hughes");
+    thing.add(bud2);
     return thing;
   }
 }
---
+++ demo/src/main/java/com/example/demo/models/Dudes.java
@@ -0,0 +1,34 @@
+package com.example.demo.models;
+
+public class Dudes {
+    private String name;
+    private String group;
+    private int age;
+    private String height;
+    
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getGroup() {
+        return group;
+    }
+    public void setGroup(String group) {
+        this.group = group;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public String getHeight() {
+        return height;
+    }
+    public void setHeight(String height) {
+        this.height = height;
+    }
+    
+}
